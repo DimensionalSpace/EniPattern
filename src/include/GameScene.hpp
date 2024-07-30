ea0f76b83b8e90cb85e5ed89783fed6289fa71df
Dimensional Space <118717619+DimensionalSpace@users.noreply.github.com>
2024-07-30 16:59:58 +0200

    Update GameScene.hpp

--- src/include/GameScene.hpp
+++ src/include/GameScene.hpp
@@ -2,7 +2,6 @@
 #include <SFML/Graphics.hpp>
 #include "./Scene.hpp"
 #include "TilemapImporter.hpp"
-#include "Player.hpp"
 #include "Goal.hpp"
 #include "PushableItem.hpp"
 #include "Teleporter.hpp"
